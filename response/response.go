08c94146df07814ac7e56143203b956b56aee53a
hewenjie <hewenjie@xiaoetech.com>
2026-02-28 20:24:16 +0800

    修改响应体结构，统一字段名称为 Message，更新错误处理信息

--- response/response.go
+++ response/response.go
@@ -7,18 +7,18 @@ import (
 )
 
 type Body struct {
-	Code int         `json:"code"`
-	Msg  string      `json:"msg"`
-	Data interface{} `json:"data,omitempty"`
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 func Response(w http.ResponseWriter, resp interface{}, err error) {
 	var body Body
 	if err != nil {
 		body.Code = -1
-		body.Msg = err.Error()
+		body.Message = err.Error()
 	} else {
-		body.Msg = "OK"
+		body.Message = "success"
 		body.Data = resp
 	}
 	httpx.OkJson(w, body)
